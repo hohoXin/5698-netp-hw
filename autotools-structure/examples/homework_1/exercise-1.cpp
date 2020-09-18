230632190f31c37c64fbea4b3b0673b8ab292254
mychele <michele.polese@gmail.com>
2020-09-18 15:09:57 -0400

    One possible solution

--- autotools-structure/examples/homework_1/exercise-1.cpp
+++ autotools-structure/examples/homework_1/exercise-1.cpp
@@ -7,6 +7,9 @@
  * Authors Filippo Campagnaro and Michele Polese 
  */
 
+// this include was missing!
+#include <iostream>
+
 struct Trivial {
   int a; 
   Trivial(int aa) : a(aa) { } 
@@ -16,30 +19,33 @@ struct Trivial {
 
 // do not modify the enumerators, find some other way
 enum NotScoped {green, blue, red};
-enum SameEnumerators {green, yellow};
+enum class SameEnumerators {green, yellow};
 
 int main(int argc, char* argv[])
 {
-  unsigned int unsigned_integer_variable {-10}
-  short short_variable {90000};
+  // there are different ways to fix this, I just picked one for this example
+  signed int unsigned_integer_variable {-10};
+  int short_variable {90000};
 
   std::cout << "unsigned_integer_variable " << unsigned_integer_variable
     << " short_variable " << short_variable << std::endl;
 
   void* pointer {&unsigned_integer_variable};
-  std::cout << "pointer content " << pointer 
-    << " value pointed by pointer " << *pointer << std::endl;
+  std::cout << "pointer content " << pointer << std::endl;
+  // cannot dereference a void* 
+    // << " value pointed by pointer " << *pointer << std::endl;
 
   const char* p = "northeastern";
   std::cout << "before modification " << *p << std::endl;
-  (*p) = 'c';
+  // if p is a const char, it cannot be modified
+  // (*p) = 'c';
   std::cout << "after modification " << *p << std::endl;
 
   Trivial structure{10};
   Trivial* ptr_struct{&structure};
 
-  std::cout << "Value in struct " << structure->a 
-    << " with pointer " << ptr_struct.a << std::endl;
+  std::cout << "Value in struct " << structure.a 
+    << " with pointer " << ptr_struct->a << std::endl;
  
   return 0;
 }
--- autotools-structure/examples/homework_1/exercise-2.cpp
+++ autotools-structure/examples/homework_1/exercise-2.cpp
@@ -6,22 +6,36 @@
  * Authors Michele Polese 
  */
 
-// remember to use the relevant #include
+#include <iostream>
 
 int main (int argc, char* argv[])
 {
+	const int size = argc - 1;
 	// declare an array of C-style strings (i.e., string literals) 
 	// which should contain the command-line arguments, except for the
 	// name of this binary
-	// the size of the array should adapt to the number of arguments
-	// passed from the command line
+	const char* array[size];
 
 	// initialize each entry in the array with the command-line arguments
 	// (except for the name of the binary, which is the first entry in argv)
+	for(int i = 1; i < argc; ++i)
+	{
+		array[i - 1] = argv[i];
+	}
 
 	// print the content of the array to the terminal, one entry for each line
 	// (without using argv!)
+	for (int i = 0; i < size; ++i)
+	{
+		std::cout << array[i] << std::endl;
+	}
 
 	// print the first string literal in the array, one letter for each line
-	return 0;
+	const char* str = array[0];
+
+	for(; *str != '\0'; ++str)
+	{
+		std::cout << *str << std::endl;
+	}
+
 }
