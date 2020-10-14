4ad97e080b37055889eb24af43088706361f8e0a
mychele <michele.polese@gmail.com>
2020-10-14 10:29:31 -0400

    Improve code

--- autotools-structure/examples/homework_2/proxy.cpp
+++ autotools-structure/examples/homework_2/proxy.cpp
@@ -25,109 +25,140 @@ int main(int argc, char** argv) {
   
   const int listen_port = argc == 1 ? 55555 : atoi(argv[1]);
   const int udp_port = argc <= 2 ? 55556 : atoi(argv[2]);
+  const char* udp_ip = "127.0.0.1";
   
-  //Opening UDP client
-  struct sockaddr_in servaddr; 
-  
+  // Open UDP client
   // Creating socket file descriptor 
-  int sockfd_udp;
-  if ( (sockfd_udp = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) { 
-      perror("socket creation failed"); 
-      return -6; 
+  int sockfd_udp = socket(AF_INET, SOCK_DGRAM, 0);
+  if (sockfd_udp < 0) { 
+      std::cout  << "UDP socket creation failed" << std::endl; 
+      return -1; 
   } 
-
-  memset(&servaddr, 0, sizeof(servaddr)); 
-    
-  // Filling server information 
-  servaddr.sin_family = AF_INET; 
-  servaddr.sin_port = htons(udp_port); 
-  //servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-if (inet_pton(AF_INET, "127.0.0.1", &servaddr.sin_addr) <= 0) { 
-    std::cout << "ERROR CONVERTING IP TO INTERNET ADDR" << std::endl;
+  
+  struct sockaddr_in servaddr_udp = {0};     
+  // Fill UDP server information 
+  servaddr_udp.sin_family = AF_INET; 
+  servaddr_udp.sin_port = htons(udp_port); 
+  if (inet_pton(AF_INET, udp_ip, &servaddr_udp.sin_addr) <= 0) { 
+    std::cout << "Conversion from char* to IP failed" << std::endl;
     close(sockfd_udp); // if conversion fail, close the socket and return error -2
     return -2;
   }
-  // open a SOCK_STREAM (TCP) socket
+
+  // Open a SOCK_STREAM (TCP) socket called scklist
   int scklist = socket(AF_INET, SOCK_STREAM, 0);
-  if (scklist < 0){ 
-    std::cout << "ERROR: OPEN SOCKET " << scklist << std::endl;
+  if (scklist < 0) { 
+    std::cout  << "TCP socket creation failed" << std::endl; 
     close(scklist);
-    return -1;
+    close(sockfd_udp);
+    return -3;
   }
-  // set socket options to release the socket address immediately after 
+
+  // Set socket options to release the socket address immediately after, on both sockets 
   // the socket is closed
   int option(1);
   setsockopt(scklist, SOL_SOCKET, SO_REUSEADDR, 
                   (char*)&option, sizeof(option));
+  setsockopt(sockfd_udp, SOL_SOCKET, SO_REUSEADDR, 
+                  (char*)&option, sizeof(option));
+
+  // bind the TCP listen socket
   struct sockaddr_in my_addr = {0}; // set all elements of the struct to 0
   my_addr.sin_family = AF_INET; // address family is AF_INET (IPV4)
-
   // convert listener_port to network number format
   my_addr.sin_port = htons(listen_port); 
   // accept connection from all interfaces of the Network Interface Card (NIC)
   my_addr.sin_addr.s_addr = htonl(INADDR_ANY); 
   // bind the socket to the port
-  if (bind(scklist,(struct sockaddr*)&my_addr, sizeof(my_addr)) < 0) { 
-    std::cout << "ERROR: BIND SOCKET" << std::endl;
+  if (bind(scklist, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0) { 
+    std::cout  << "TCP socket bind failed" << std::endl; 
     close(scklist);
-    return -2;
+    close(sockfd_udp);
+    return -4;
   }
 
   // Accept at max 1 client at a time: by changing 1 to 5 in the next line 
   // you accept at most the connection of 5 clients in parallel.
   if (listen(scklist, 1) < 0) { 
-    std::cout << "ERROR: LISTEN SOCKET" << std::endl;
+    std::cout  << "TCP socket listen failed" << std::endl; 
     close(scklist);
-    return -3;
+    close(sockfd_udp);
+    return -5;
   }
 
-  struct sockaddr_in client_addr;
-  socklen_t addr_l = sizeof(client_addr);
+  // Create a sockaddr_in for clientaddr_tcp
+  struct sockaddr_in clientaddr_tcp;
+  socklen_t addr_l = sizeof(clientaddr_tcp);
 
   // the next line does 3 operations:
   // 1) it accepts the connection of a new client.
   // 2) it creates a new socket used to perform the actual data transmission 
   //    between client ad server. If the socket creation fails, it returns -1.
-  // 3) it Saves into client_addr the address of the client connected, so you 
+  // 3) it Saves into clientaddr_tcp the address of the client connected, so you 
   //    can know who is sending what.
-  int sockfd = accept(scklist, (struct sockaddr*) &client_addr, &addr_l);
+  int sockfd = accept(scklist, (struct sockaddr*) &clientaddr_tcp, &addr_l);
   if(sockfd < 0) {
-    std::cout << "ERROR: ACCEPT CONNECTION" << std::endl;
+    std::cout  << "TCP socket accept failed" << std::endl; 
     close(sockfd);
     close(scklist);
-    return -4;
+    close(sockfd_udp);
+    return -6;
   }
 
   // The next line prints the client address, converting to char* 
   // the network address (inet_ntoa)
-  std::cout << "New connection from " << inet_ntoa(client_addr.sin_addr) << std::endl; 
+  std::cout << "New connection from " << inet_ntoa(clientaddr_tcp.sin_addr) << std::endl; 
 
-  // receive from sockfd
-  const size_t max_size = 256;
+  // Prepare the receive buffer, with at most 512 bytes
+  const size_t max_size = 512;
   char buf[max_size] = {0};
 
-  while(1){
-    memset(buf, 0, sizeof(buf)); 
+  // Create a while(true) loop
+  while(true) {
+    // Set the buf memory to 0
+    memset(buf, 0, sizeof(buf));
+
+    // Receive from the TCP sockfd (not the listening socket)
     int rcv_size = recv(sockfd, buf, max_size, 0);
+
+    // Check if the receive was correct
     if(rcv_size < 0) {
-      std::cout << "ERROR: RECV" << std::endl;
+      std::cout  << "TCP socket receive failed" << std::endl; 
       close(sockfd);
       close(scklist);
-      return -5;
+      close(sockfd_udp);
+      return -7;
+    } else if (rcv_size == 0) {
+      // this means that the socket has been closed
+      std::cout  << "TCP socket closed" << std::endl; 
+      close(sockfd);
+      close(scklist);
+      close(sockfd_udp);
+      return 0;
     }
+
+    // Print the buffer and the received size
     std::cout << "Received: " << buf << std::endl;
     std::cout << "Size: " << rcv_size << std::endl;
-    /////////---------UDP Client sending data----------//////////
 
+    // Send buf on sockfd_udp using sendto
+    int sent_size = sendto(sockfd_udp, (const char *)buf, strlen(buf), 0, 
+      (const struct sockaddr *) &servaddr_udp, sizeof(servaddr_udp)); 
 
-    sendto(sockfd_udp, (const char *)buf, strlen(buf), MSG_CONFIRM, 
-      (const struct sockaddr *) &servaddr, sizeof(servaddr)); 
+    // Check if the send was correct
+    if(sent_size <= 0) {
+      std::cout  << "UDP socket send failed" << std::endl; 
+      close(sockfd);
+      close(scklist);
+      close(sockfd_udp);
+      return -8;
+    }
 
-    printf("message sent to udp server.\n"); 
+    std::cout << "Message sent to udp server" << std::endl; 
   }
+
   // close the sockets
   close(sockfd);
-    close(scklist);
-
+  close(scklist);
   close(sockfd_udp);
 }
