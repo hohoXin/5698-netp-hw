c724df9a6f142f79cacf2b3bff897cf59ef99895
mychele <michele.polese@gmail.com>
2022-09-08 13:06:21 -0400

    First example

---
+++ autotools-structure/examples/lesson_1_hello_world/hello-world.cpp
@@ -0,0 +1,15 @@
+/*
+ * [2020] Northeastern University - Network Programming
+ * All Rights Reserved.
+ * 
+ * Authors Michele Polese 
+*/
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{  
+  // this is a comment, not executed
+  std::cout << "Hello, World" << std::endl;
+  return 0; 
+}
