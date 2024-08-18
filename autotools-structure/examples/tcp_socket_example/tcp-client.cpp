613195b9232beb22a0a1ee4797ffbbeb44759f49
hohoXin <zxz142857@outlook.com>
2024-08-18 21:45:11 +0000

    XZ_lab2

--- autotools-structure/examples/tcp_socket_example/tcp-client.cpp
+++ autotools-structure/examples/tcp_socket_example/tcp-client.cpp
@@ -27,13 +27,37 @@ int main(int argc, char** argv)
   // "127.0.0.1" if no argument is provided, or the first argument otherwise
   // exercise 2: declare a variable dest_port of type const int, which is
   // 55555 if no argument is provided, or the second argument otherwise
+  // if(argc==1){
+  //   const char* dest_ip = "127.0.0.1";
+  //   const int dest_port = 55555;
+  // }
+  // else if(argc==2){
+  //   const int dest_port = 55555;
+  //   const char* dest_ip = argv[1];
+  // }
+  // else{
+  //   const char* dest_ip = argv[1];
+  //   const int dest_port = atoi(argv[2]);
+  // }
+
+  const char* dest_ip = argc == 1 ? "127.0.0.1" : argv[1];
+  const int dest_port = argc > 2 ? atoi(argv[2]) : 55555;
 
   // exercise 2: open a SOCK_STREAM (TCP) socket and save the file descriptor
   // in an int variable sckfd
   // check if the operation is successful, if not, print an error, close the socket and return
+  int sckfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sckfd<0){
+    /*open sckfd ERROR*/
+    close(sckfd);
+    std::cout << "sckfd < 0, client socket open failed" << std::endl;
+    return -1;
+  }
 
   // exercise 3: set socket options to release the socket address immediately after 
   // the socket is closed, so that it can be immediately reused (see SO_REUSEADDR and setsockopt)
+  int option(1);
+  setsockopt(sckfd, SOL_SOCKET, SO_REUSEADDR, (char*)&option, sizeof(option));
 
   // exercise 4
   // declare and set to 0 a struct called dest_addr and of type struct sockaddr_in 
@@ -42,10 +66,25 @@ int main(int argc, char** argv)
   // tip: inet_pton(AF_INET, dest_ip, and a pointer to dest_addr.sin_addr)
   // check if the conversion is successful (according to the return of inet_pton),
   // if not, print an error, close the socket and return
+  struct sockaddr_in dest_addr = {0};
+  dest_addr.sin_family = AF_INET;
+  dest_addr.sin_port = htons(dest_port);
+  if(inet_pton(AF_INET, dest_ip, &dest_addr.sin_addr)<0){
+    /*conversion inet_pton ERROR*/
+    close(sckfd);
+    std::cout << "conversion dest_ip to AF_ADDRESS failed" << std::endl;
+    return -2;
+  }
 
   // exercise 5 connect the socket to the server ip:port specified into dest_addr
   // using the connect API on sckfd
   // check if the operation is successful, if not, print an error, close the socket and return
+  if(connect(sckfd, (struct sockaddr*) &dest_addr, sizeof(dest_addr))<0){
+    /*connect ERROR*/
+    close(sckfd);
+    std::cout << "connect socket to ip failed" << std::endl;
+    return -3;
+  }
 
   // transmit this data
   const size_t max_size = 512;
@@ -55,15 +94,30 @@ int main(int argc, char** argv)
 
   // exercise 6 use the send API on sckfd to transmit the buf
   // check if the operation is successful, if not, print an error, close the socket and return
+  int sent_size = send(sckfd, buf, data_size, 0);
+  if(sent_size < 0){
+    /*sent message ERROR*/
+    close(sckfd);
+    std::cout << "transmit the buf with send failed" << std::endl;
+    return -4;
+  }
 
   // set buffer to zero for next read
   memset(buf, 0, max_size);
 
   // exercise 7 wait to receive data with the recv API on sckfd
   // check if the operation is successful, if not, print an error, close the socket and return
+  int rcv_size = recv(sckfd, buf, max_size, 0);
+  if(rcv_size < 0){
+    /*receive data ERROR*/
+    close(sckfd);
+    std::cout << "recieved the data with recv failed" << std::endl;
+    return -5;
+  }
 
   std::cout << buf << std::endl;
   
   // exercise 8 close the socket
+  close(sckfd);
 }
 
--- autotools-structure/examples/tcp_socket_example/tcp-server.cpp
+++ autotools-structure/examples/tcp_socket_example/tcp-server.cpp
@@ -31,6 +31,18 @@ void invertBuffer(char* buf, size_t size)
   // - pay attention to the index
   //   Do not place buffer[size - 1] as first, because this is a 
   //   zero-terminated string
+
+  // std::cout << "buf[size-1]: " << buf[size-1] << std::endl; // buf[size-1] is '\0'
+  // std::cout << "buf[size-2]: " << buf[size-2] << std::endl; // buf[size-1] is last char
+
+  char tmp;
+  for(size_t i = 0; 2 * i < size; i++)
+  {
+    tmp = buf[i];
+    buf[i] = buf[size-2-i];
+    buf[size-2-i]=tmp;
+    // std::cout << "debugging #" << i+1 << "'s buf is " << buf << std::endl;
+  }
 }
 
 int main(int argc, char** argv) {
@@ -43,30 +55,58 @@ int main(int argc, char** argv) {
   // exercise 1: declare a variable listen_port of type const int, which is
   // 55555 if no argument is provided, or the argument otherwise
   // as for the UDP lab, skip this and declare const int listen_port {55555}; if you are not sure of how to do it
-  
+  const int listen_port = argc == 1 ? 55555 : atoi(argv[1]);
+
   // exercise 2: open a SOCK_STREAM (TCP) socket and save the file descriptor
   // in an int variable scklist
   // check if the operation is successful, if not, print an error, close the socket and return
+  int scklist = socket(AF_INET, SOCK_STREAM, 0);
+  if(scklist<0){
+    /*socket file descriptor ERROR*/
+    close(scklist);
+    std::cout << "socket file descriptor < 0" << std::endl;
+    return -1; 
+  }
 
   // exercise 3: set socket options to release the socket address immediately after 
   // the socket is closed, so that it can be immediately reused (see SO_REUSEADDR and setsockopt from Friday's class)
-  
+  int option(1);
+  setsockopt(scklist, SOL_SOCKET, SO_REUSEADDR, (char*)&option, sizeof(option));
+
   // exercise 4
   // declare and set to 0 a struct called my_addr and of type struct sockaddr_in 
   // then set the field sin_family of my_addr to AF_INET, the sin_port to listen_port
   // and the sin_addr.s_addr to INADDR_ANY (remember the conversions from network to host)
-  
+  struct sockaddr_in my_addr = {0};
+  my_addr.sin_family = AF_INET;
+  my_addr.sin_port = htons(listen_port);
+  my_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
   // exercise 5
   // bind the socket to my_addr using the bind API
   // check if the bind is successful, if not, print an error, close the socket and return
- 
+  if(bind(scklist, (struct sockaddr*)&my_addr, sizeof(my_addr)) < 0){
+    /*binding ERROR*/
+    close(scklist);
+    std::cout << "bind socket address failed" << std::endl;
+    return -2;
+  }
+
   // exercise 6: set scklist in a listening state, using the listen API (with at most 1 client)
   // check if the listen is successful, if not, print an error, close the socket and return
+  if(listen(scklist, 1) < 0){
+    /*set listening ERROR*/
+    close(scklist);
+    std::cout << "set listening state failed" << std::endl;
+    return -3;
+  }
 
   // exercise 7
   // declare a struct called client_addr and of type struct sockaddr_in
   // declare a variable addr_l of type socklen_t, and initialize it with 
   // the size of srcaddr (you can use sizeof)
+  struct sockaddr_in client_addr;
+  socklen_t addr_l = sizeof(client_addr);
 
   // exercise 8
   // performs these 3 operations using the accept API on scklist:
@@ -76,17 +116,39 @@ int main(int argc, char** argv) {
   // 3) save into client_addr the address of the client connected, so you 
   //    can know who is sending what.
   // check if the accept is successful, if not, print an error, close the socket(s) and return
+  int sockfd = accept( scklist, (struct sockaddr*) &client_addr, &addr_l);
+  if(sockfd < 0){
+    /*accept ERROR*/
+    close(sockfd);
+    close(scklist);
+    std::cout << "sockfd < 0" << std::endl;
+    return -4;
+  }
 
   // exercise 8
   // use inet_ntoa on the member sin_addr of client_addr to get the IP address in 
   // the dotted decimanl notation, in a const char* variable, and print to the terminal
+  const char* client_address = inet_ntoa(client_addr.sin_addr);
+  std::cout << " connection from " << client_address << std::endl;
 
   // exercise 9: receive from sockfd
   // create a buffer called buf, with max_size set to 256
   // receive with recv on sockfd, and save the received amount of bytes in rcv_size
   // check if the recv is successful, if not, print an error, close the socket(s) and return
   // print the received buffer and size
-  
+  const size_t max_size = 256;
+  char buf[max_size] = {0};
+  int rcv_size = recv( sockfd, buf, max_size, 0);
+  if(rcv_size < 0){
+    /*recv ERROR*/
+    close(sockfd);
+    close(scklist);
+    std::cout << "receive from sockfd failed" << std::endl;
+    return -5;
+  }
+  std::cout << "recieved buffer content: " << buf << "recieved buffer size = " << sizeof(buf) << std::endl;
+  // no need for endl after the buf, for the content typed in client already has one
+
   /**
    * exercise 10
    * send back to the client whatever you received, 
@@ -96,11 +158,22 @@ int main(int argc, char** argv) {
    */
 
   // call invert buffer (uncomment this line when you want to test the implementation)
-  // invertBuffer(buf,rcv_size);
+  invertBuffer(buf,rcv_size);
 
   // call the send API on sockfd
   // check if the send is successful, if not, print an error, close the socket(s) and return
+  int sent_size = send(sockfd, buf, rcv_size, 0);
+  if(sent_size < 0){
+    /*sent ERROR*/
+    close(sockfd);
+    close(scklist);
+    std::cout << "send to the client failed" << std::endl;
+    return -6;
+  }
 
   // exercise 11: close the sockets
+  close(sockfd);
+  close(scklist);
 
+  return 0;
 }
